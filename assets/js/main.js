19428bd214e217d5a89979ac659ac63246a7bda0
ZtenBlack <vladislav@terrasoft.net>
2025-12-12 19:41:24 +0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,6 +95,7 @@ $(document).ready(function () {
         allBoxes.not(clickedBox).addClass('hidden');
         $('.page__welcome').addClass('no-bg');
         $('.hide-after-anim').slideUp();
+        $('#snow').hide();
         playOpeningSound();
         let options = [10, 15, 20];
         let extra = options[Math.floor(Math.random() * options.length)];
@@ -194,3 +195,4 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 });
+
